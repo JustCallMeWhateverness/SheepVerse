536c079005605b9e25826ce60236264f79216bc4
Whateverness <nicolefritz00@gmail.com>
2025-01-21 11:10:48 +0100

    Just some SheepChanges

--- sheep_data.js
+++ sheep_data.js
@@ -1,19 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.sheep-card');
 
-    const sheepCards = document.querySelectorAll('.sheep-card');
-
-    sheepCards.forEach(card => {
-        // Add a click event listener to each card
-        card.addEventListener('click', function() {
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
             const details = card.querySelectorAll('p');
-
-            // Toggle the visibility of the clicked card's details
             details.forEach(detail => {
-                if (detail.style.display === 'none' || detail.style.display === '') {
-                    detail.style.display = 'block'; // Show the details
-                } else {
-                    detail.style.display = 'none';  // Hide the details
-                }
+                detail.style.display = detail.style.display === 'none' ? 'block' : 'none';
             });
         });
     });
